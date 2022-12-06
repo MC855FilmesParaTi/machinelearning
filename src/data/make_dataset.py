a1c2f6a2729f9cdf9a6ac798a558e3912e3025f9
mmedicina <marcelamedicinaf@gmail.com>
2022-12-06 00:11:02 -0300

    gerador de datasets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,18 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import tensorflow as tf
+from reco_utils.dataset import movielens
+from reco_utils.common.constants import SEED as DEFAULT_SEED
+from reco_utils.dataset.python_splitters import python_chrono_split
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+print('Digite o tamanho do dataset:100k, 1m, 10m, ou 20m')
+MOVIELENS_DATA_SIZE = input()
+print('Digite no formato float a porcentagem de dados para treino')
+PERCENTAGE = input()
+print(MOVIELENS_DATA_SIZE,PERCENTAGE)
+PERCENTAGE = float(PERCENTAGE)
+# Loading movie leans data
+df = movielens.load_pandas_df(size=MOVIELENS_DATA_SIZE,header=["userID", "itemID", "rating", "timestamp"])
+train, test = python_chrono_split(df, PERCENTAGE)
+df.to_csv('./movielens'+MOVIELENS_DATA_SIZE+'.csv')
+train.to_csv('../features/train_'+MOVIELENS_DATA_SIZE+'.csv')
+test.to_csv('../features/test_'+MOVIELENS_DATA_SIZE+'.csv')
